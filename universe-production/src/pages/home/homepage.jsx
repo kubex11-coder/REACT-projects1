a822e5c89d34157635afb8d3582bc647ca36295e
kubex11-coder <jakubdvorak91@seznam.cz>
2024-02-26 16:20:47 +0100

    zkoušky animací na homepage

--- universe-production/src/pages/home/homepage.jsx
+++ universe-production/src/pages/home/homepage.jsx
@@ -1,7 +1,26 @@
 import React from "react"
+import "./homepage.css"
+import SUN from "../../images/SUN.png"
+import { motion } from "framer-motion"
+import Planet1 from "../../images/planet1.png"
 
 const homepage = () => {
-    return <div>homepage</div>
+    return (
+        <div className="homepage">
+            <div className="logo-section">Logo</div>
+            <motion.div
+                className="sun"
+                animate={{ scale: 2.1 }}
+                transition={{ duration: 30 }}
+            >
+                <img src={SUN} alt="" />
+            </motion.div>
+            <motion.div className="planet1">
+                <img src={Planet1} alt="" />
+            </motion.div>
+            <div className="cosmonaut">cosmonaut</div>
+        </div>
+    )
 }
 
 export default homepage
